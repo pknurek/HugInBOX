a8cbf4de9099d5e38f61eb3546b1641e4765f515
pknurek <pknurek@outlook.com>
2025-10-02 14:58:30 +0200

    ProductDatabase.kt

---
+++ ProductDatabase.kt
@@ -0,0 +1,27 @@
+package com.huginbox.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Product::class], version = 1)
+abstract class ProductDatabase : RoomDatabase() {
+    abstract fun productDao(): ProductDao
+
+    companion object {
+        @Volatile private var INSTANCE: ProductDatabase? = null
+
+        fun getDatabase(context: Context): ProductDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    ProductDatabase::class.java,
+                    "product_database"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
